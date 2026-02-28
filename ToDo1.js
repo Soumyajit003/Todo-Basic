36d646b18d61fbdafdc043f50c4f7aaf286e2093
Soumyajit003 <soumya.bera003@gmail.com>
2026-03-01 04:36:21 +0530

    feat: Implement a functional ToDo application with task management, styling, and local storage persistence.

---
+++ ToDo1.js
@@ -0,0 +1,175 @@
+const submit = document.querySelector("#submit");
+const container = document.querySelector(".container");
+const inp = document.querySelector("#inp");
+const submitDiv = document.querySelector(".submitDiv");
+let f = 0;
+let ent = 0;
+
+const arr = [];
+
+submit.addEventListener("click", () => {
+  if (inp.value != "") {
+    const obj = {};
+    obj.task = inp.value;
+    obj.id = null;
+    obj.status = "pending";
+    taskAdd(obj);
+    inp.value = "";
+  }
+});
+inp.addEventListener("keypress", (enter) => {
+  if (ent == 0) {
+    if (enter.key === "Enter" && inp.value != "") {
+      const obj = {};
+      obj.task = inp.value;
+      obj.id = null;
+      obj.status = "pending";
+      taskAdd(obj);
+      inp.value = "";
+    }
+  }
+});
+
+function taskAdd(obj) {
+  let id = Date.now();
+  const done = document.createElement("button");
+  done.innerHTML = "";
+  done.classList = "done btn";
+  const cross = document.createElement("button");
+  cross.innerHTML = "&cross;";
+  cross.classList = "cross btn";
+  const span = document.createElement("span");
+  span.classList = "span";
+  span.style = "cursor:pointer;";
+  const edit = document.createElement("button");
+  edit.classList = "edit-btn";
+  edit.innerHTML = "Edit";
+
+  const save = document.createElement("button");
+  save.innerHTML = "Save";
+  save.classList = "btn";
+  save.id = "save";
+
+  const task = document.createElement("div");
+  task.classList = "task";
+  task.id = id;
+
+  if(obj.status == "successfull"){
+    span.style.textDecoration = "line-through";
+    done.classList.add("checked");
+    span.classList.add("taskDone");
+  }
+
+  task.append(done, span, edit, cross);
+
+  span.innerText = obj.task;
+  obj.id = id;
+  arr.push(obj);
+  console.log(arr);
+  container.append(task);
+
+  // <================ Done Button=================>
+  done.addEventListener("click", (d) => {
+    if (
+      span.style.textDecoration != "line-through" &&
+      span.style.textDecorationColor != "var(--btn_color)"
+    ) {
+      span.style.textDecoration = "line-through";
+      obj.status = "successfull";
+      saveData(arr);
+    } else {
+      span.style.textDecoration = "none";
+      span.style.textDecorationColor = "transparent";
+      obj.status = "pending";
+      saveData(arr);
+    }
+    done.classList.toggle("checked");
+    span.classList.toggle("taskDone");
+    obj.status = "successfull";
+  });
+
+  // <================ Span Task =================>
+  span.addEventListener("click", (d) => {
+    if (
+      span.style.textDecoration != "line-through" &&
+      span.style.textDecorationColor != "var(--btn_color)"
+    ) {
+      span.style.textDecoration = "line-through";
+      obj.status = "successfull";
+      saveData(arr);
+    } else {
+      span.style.textDecoration = "none";
+      span.style.textDecorationColor = "transparent";
+      obj.status = "pending";
+      saveData(arr);
+    }
+    done.classList.toggle("checked");
+    span.classList.toggle("taskDone");
+  });
+
+  // <================ Edit Task =================>
+  let id2;
+  edit.addEventListener("click", (e) => {
+    if (f == 1) {
+      window.alert("Please save the task first !!!");
+    }
+    if (f == 0) {
+      id2 = obj.id;
+      inp.value = span.innerHTML;
+      submit.remove();
+      submitDiv.append(save);
+      f = 1;
+      ent = 1;
+    }
+  });
+
+  // <================ remove Task =================>
+  cross.addEventListener("click", (c) => {
+    let id1 = c.target.parentNode.id;
+    c.target.parentElement.remove();
+    let temp = arr.findIndex((user) => user.id == id1);
+    arr.splice(temp, 1);
+    localStorage.setItem("User", JSON.stringify(arr));
+  });
+
+  // <================ Save Task =================>
+  save.addEventListener("click", () => {
+    if (inp.value == "") {
+      window.alert("Please enter tesk!!!");
+    } else {
+      span.innerHTML = inp.value;
+      console.log(id2);
+      console.log(inp.value);
+
+      arr.forEach((obj) => {
+        if (obj.id == id2) {
+          obj.task = inp.value;
+        }
+      });      
+      inp.value = "";
+      console.log(arr);
+      save.remove();
+      submitDiv.append(submit);
+      f = 0;
+      ent = 0;
+      saveData(arr);
+    }
+  });
+
+  saveData(arr);
+}
+
+// <================ Save Task in local storage =================>
+function saveData(arr) {
+  localStorage.setItem("User", JSON.stringify(arr));
+}
+
+window.addEventListener("load", showData);
+
+// <================ Show Task after reload =================>
+function showData() {
+  let t = JSON.parse(localStorage.getItem("User"));
+  t.forEach((obj) => {
+    taskAdd(obj);
+  });
+}
